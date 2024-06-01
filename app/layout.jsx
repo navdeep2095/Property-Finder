1f4824531e2218e750dd6c9fe3b515a1f17e31e1
Navdeep Singh <singhnavdeep2095@gmail.com>
2024-06-01 21:51:37 +0530

    Worked on homepage, layout and metadata for homepage

---
+++ app/layout.jsx
@@ -0,0 +1,18 @@
+import '@/assets/styles/globals.css';
+export const metadata = {
+    type: 'Property details | find the right property',
+    description: 'find your dream property',
+    keywords: 'find rentals, properties, home, home sweet home'
+}
+const MainLayout = ({children}) => {
+  return (
+    <html lang="en">
+        <body>
+            
+        <div>{children}</div>
+        </body>
+    </html>
+  )
+}
+
+export default MainLayout
